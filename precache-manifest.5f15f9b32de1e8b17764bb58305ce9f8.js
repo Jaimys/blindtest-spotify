06034126f5421466fe4b310ad3f7582d5a11f20b
Félix Gander <Felix.Gander@grenoble-inp.org>
2022-09-22 08:14:02 +0200

    Updates

---
+++ precache-manifest.5f15f9b32de1e8b17764bb58305ce9f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13c1a3ee3c984cba8b311176c0aaff5",
+    "url": "/blindtest-spotify/index.html"
+  },
+  {
+    "revision": "7d225952818a30b49445",
+    "url": "/blindtest-spotify/static/css/main.edb96105.chunk.css"
+  },
+  {
+    "revision": "c216a6afe98261ca4825",
+    "url": "/blindtest-spotify/static/js/2.45d35889.chunk.js"
+  },
+  {
+    "revision": "5abf8b84c7663e5059f2ad6310e20abb",
+    "url": "/blindtest-spotify/static/js/2.45d35889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d225952818a30b49445",
+    "url": "/blindtest-spotify/static/js/main.dcae0943.chunk.js"
+  },
+  {
+    "revision": "aa4bec96cd494a3eb558",
+    "url": "/blindtest-spotify/static/js/runtime-main.0b05215a.js"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  }
+]);
